b643102d70a6b75a5050c86ebba294f9141c9e80
armandolopezg <coredrkv2@gmail.com>
2022-10-12 11:49:18 -0500

    ola

---
+++ cenafilofos/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace cenafilofos
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.palillo1 = new System.Windows.Forms.Label();
+            this.palillo4 = new System.Windows.Forms.Label();
+            this.palillo2 = new System.Windows.Forms.Label();
+            this.palillo3 = new System.Windows.Forms.Label();
+            this.palillo5 = new System.Windows.Forms.Label();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cena Filosofos";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(41, 95);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(102, 95);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(470, 95);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(102, 95);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(275, 95);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(102, 95);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(25, 296);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(102, 95);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(470, 283);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(102, 95);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 5;
+            this.pictureBox5.TabStop = false;
+            // 
+            // palillo1
+            // 
+            this.palillo1.AutoSize = true;
+            this.palillo1.ForeColor = System.Drawing.Color.Black;
+            this.palillo1.Location = new System.Drawing.Point(149, 135);
+            this.palillo1.Name = "palillo1";
+            this.palillo1.Size = new System.Drawing.Size(61, 20);
+            this.palillo1.TabIndex = 7;
+            this.palillo1.Text = "Palillo 1";
+            // 
+            // palillo4
+            // 
+            this.palillo4.AutoSize = true;
+            this.palillo4.ForeColor = System.Drawing.Color.Black;
+            this.palillo4.Location = new System.Drawing.Point(297, 358);
+            this.palillo4.Name = "palillo4";
+            this.palillo4.Size = new System.Drawing.Size(61, 20);
+            this.palillo4.TabIndex = 8;
+            this.palillo4.Text = "Palillo 4";
+            // 
+            // palillo2
+            // 
+            this.palillo2.AutoSize = true;
+            this.palillo2.ForeColor = System.Drawing.Color.Black;
+            this.palillo2.Location = new System.Drawing.Point(383, 135);
+            this.palillo2.Name = "palillo2";
+            this.palillo2.Size = new System.Drawing.Size(61, 20);
+            this.palillo2.TabIndex = 9;
+            this.palillo2.Text = "Palillo 2";
+            // 
+            // palillo3
+            // 
+            this.palillo3.AutoSize = true;
+            this.palillo3.ForeColor = System.Drawing.Color.Black;
+            this.palillo3.Location = new System.Drawing.Point(493, 217);
+            this.palillo3.Name = "palillo3";
+            this.palillo3.Size = new System.Drawing.Size(61, 20);
+            this.palillo3.TabIndex = 10;
+            this.palillo3.Text = "Palillo 3";
+            // 
+            // palillo5
+            // 
+            this.palillo5.AutoSize = true;
+            this.palillo5.ForeColor = System.Drawing.Color.Black;
+            this.palillo5.Location = new System.Drawing.Point(41, 232);
+            this.palillo5.Name = "palillo5";
+            this.palillo5.Size = new System.Drawing.Size(61, 20);
+            this.palillo5.TabIndex = 11;
+            this.palillo5.Text = "Palillo 5";
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
+            this.pictureBox6.Location = new System.Drawing.Point(264, 271);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(125, 62);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 12;
+            this.pictureBox6.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(283, 401);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Iniciar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(51, 62);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(61, 20);
+            this.lbl1.TabIndex = 14;
+            this.lbl1.Text = "Comida";
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Location = new System.Drawing.Point(51, 410);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(61, 20);
+            this.lbl4.TabIndex = 15;
+            this.lbl4.Text = "Comida";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Location = new System.Drawing.Point(493, 52);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(61, 20);
+            this.lbl3.TabIndex = 16;
+            this.lbl3.Text = "Comida";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(297, 52);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(61, 20);
+            this.lbl2.TabIndex = 17;
+            this.lbl2.Text = "Comida";
+            // 
+            // lbl5
+            // 
+            this.lbl5.AutoSize = true;
+            this.lbl5.Location = new System.Drawing.Point(493, 401);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(61, 20);
+            this.lbl5.TabIndex = 18;
+            this.lbl5.Text = "Comida";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 492);
+            this.Controls.Add(this.lbl5);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.palillo5);
+            this.Controls.Add(this.palillo3);
+            this.Controls.Add(this.palillo2);
+            this.Controls.Add(this.palillo4);
+            this.Controls.Add(this.palillo1);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private Label palillo1;
+        private Label palillo4;
+        private Label palillo2;
+        private Label palillo3;
+        private Label palillo5;
+        private PictureBox pictureBox6;
+        private Button button1;
+        private Label lbl1;
+        private Label lbl4;
+        private Label lbl3;
+        private Label lbl2;
+        private Label lbl5;
+    }
+}
---
+++ cenafilofos/Form1.cs
@@ -0,0 +1,244 @@
+using cenafilosofos;
+using System.Threading;
+
+namespace cenafilofos
+{
+    public partial class Form1 : Form
+    {
+        static sticks[] p = new sticks[5];
+
+       static int comidas1 = 0;
+       static int comidas2 = 0;
+       static int comidas3 = 0;
+       static int comidas4 = 0;
+       static int comidas5 = 0;
+
+
+        static Semaphore knuckles = new Semaphore (2,3);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < p.Length; i++)
+            {
+                p[i] = new sticks();
+                p[i].id = i + 1;
+
+            }
+
+            ACenaLeFascina();
+        }
+
+        private void ACenaLeFascina()
+        {
+            Thread thread1 = new Thread(TeAmoDrake1);
+            thread1.Start();
+
+            Thread thread2 = new Thread(TeAmoDrake2);
+            thread2.Start();
+
+            Thread thread3 = new Thread(TeAmoDrake3);
+            thread3.Start();
+
+            Thread thread4 = new Thread(TeAmoDrake4);
+            thread4.Start();
+
+
+            Thread thread5 = new Thread(TeAmoDrake5);
+            thread5.Start();
+
+
+        }
+
+        private void TeAmoDrake1()
+        {
+
+            knuckles.WaitOne();
+            if (comidas1 == 5) 
+            {
+              
+                Invoke(new MethodInvoker(() =>
+                {
+                    lbl1.Text = "El filosofo se la comio";
+                }));
+            }
+
+            else
+            {
+                if (palillo1.ForeColor == Color.Black && palillo5.ForeColor == Color.Black)
+                {
+                    Invoke(new MethodInvoker(() =>
+                    {
+                        palillo1.ForeColor = Color.Red;
+                        palillo5.ForeColor = Color.Red;
+
+                    }));
+
+                    comidas1++;
+                    Thread.Sleep(5000);
+                    Invoke(new MethodInvoker(() =>
+                    {
+                        palillo1.ForeColor = Color.Black;
+                        palillo5.ForeColor = Color.Black;
+                        lbl1.Text = comidas1.ToString();
+                    }));
+                }
+         
+            } knuckles.Release();
+        }
+
+        private void TeAmoDrake2()
+        {
+
+            knuckles.WaitOne();
+            if (comidas2 == 5)
+            {
+
+                Invoke(new MethodInvoker(() =>
+                {
+                    lbl2.Text = "El filosofo se la comio";
+                }));
+            }
+
+            else
+            {
+                if (palillo2.ForeColor == Color.Black && palillo1.ForeColor == Color.Black)
+                {
+                    Invoke(new MethodInvoker(() =>
+                    {
+                        palillo2.ForeColor = Color.Blue;
+                        palillo1.ForeColor = Color.Blue;
+
+                    }));
+
+                    comidas2++;
+                    Thread.Sleep(5000);
+                    Invoke(new MethodInvoker(() =>
+                    {
+                        palillo2.ForeColor = Color.Black;
+                        palillo1.ForeColor = Color.Black;
+                        lbl2.Text = comidas2.ToString();
+                    }));
+                }
+
+            } knuckles.Release();
+        }
+
+
+        private void TeAmoDrake3()
+        {
+
+            knuckles.WaitOne();
+            if (comidas3 == 5)
+            {
+
+                Invoke(new MethodInvoker(() =>
+                {
+                    lbl3.Text = "El filosofo se la comio";
+                }));
+            }
+
+            else
+            {
+                if (palillo3.ForeColor == Color.Black && palillo2.ForeColor == Color.Black)
+                {
+                    Invoke(new MethodInvoker(() =>
+                    {
+                        palillo3.ForeColor = Color.Green;
+                        palillo2.ForeColor = Color.Green;
+
+                    }));
+
+                    comidas3++;
+                    Thread.Sleep(5000);
+                    Invoke(new MethodInvoker(() =>
+                    {
+                        palillo3.ForeColor = Color.Black;
+                        palillo2.ForeColor = Color.Black;
+                        lbl3.Text = comidas3.ToString();
+                    }));
+                }
+
+            } knuckles.Release();
+        }
+
+        private void TeAmoDrake4()
+        {
+
+            knuckles.WaitOne();
+            if (comidas4 == 5)
+            {
+
+                Invoke(new MethodInvoker(() =>
+                {
+                    lbl4.Text = "El filosofo se la comio";
+                }));
+            }
+
+            else
+            {
+                if (palillo4.ForeColor == Color.Black && palillo3.ForeColor == Color.Black)
+                {
+                    Invoke(new MethodInvoker(() =>
+                    {
+                        palillo4.ForeColor = Color.Yellow;
+                        palillo3.ForeColor = Color.Yellow;
+
+                    }));
+
+                    comidas4++;
+                    Thread.Sleep(5000);
+                    Invoke(new MethodInvoker(() =>
+                    {
+                        palillo4.ForeColor = Color.Black;
+                        palillo3.ForeColor = Color.Black;
+                        lbl4.Text = comidas4.ToString();
+                    }));
+                }
+
+            } knuckles.Release();
+        }
+
+
+        private void TeAmoDrake5()
+        {
+
+            knuckles.WaitOne();
+            if (comidas5 == 5)
+            {
+
+                Invoke(new MethodInvoker(() =>
+                {
+                    lbl5.Text = "El filosofo se la comio";
+                }));
+            }
+
+            else
+            {
+                if (palillo5.ForeColor == Color.Black && palillo4.ForeColor == Color.Black)
+                {
+                    Invoke(new MethodInvoker(() =>
+                    {
+                        palillo5.ForeColor = Color.Violet;
+                        palillo4.ForeColor = Color.Violet;
+
+                    }));
+
+                    comidas5++;
+                    Thread.Sleep(5000);
+                    Invoke(new MethodInvoker(() =>
+                    {
+                        palillo5.ForeColor = Color.Black;
+                        palillo4.ForeColor = Color.Black;
+                        lbl5.Text = comidas5.ToString();
+                    }));
+                }
+
+            } knuckles.Release();
+        }
+
+    }
+}
